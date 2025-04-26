6ba6fd96a4f33cab6040df78587d9311c1cee3f5
mimi015 <mdavi5086@365.elmhurst.edu>
2025-04-26 14:07:28 -0500

    added other map

---
+++ CGE303Project5/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float moveSpeed = 2f;
+
+    private Vector3 nextPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPosition = pointB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+
+       if(transform.position == nextPosition) {
+              nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+       }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+
+        if (collision.gameObject.CompareTag("Player2"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+
+        if (collision.gameObject.CompareTag("Player2"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
